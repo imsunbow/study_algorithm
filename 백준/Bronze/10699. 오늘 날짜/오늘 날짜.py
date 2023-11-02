c88192127b954dacda92f56cb87dc5ad94a1581d
imsunbow <145563331+imsunbow@users.noreply.github.com>
2023-11-03 00:29:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 64 ms, Memory: 38036 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -0,0 +1,11 @@
+import datetime
+
+# 현재 날짜와 시간을 가져옵니다
+current_datetime = datetime.datetime.now()
+
+# 9시간을 더함
+new_datetime = current_datetime + datetime.timedelta(hours=9)
+
+# "YYYY-MM-DD" 형식으로 출력
+formatted_date = new_datetime.strftime("%Y-%m-%d")
+print(formatted_date)
