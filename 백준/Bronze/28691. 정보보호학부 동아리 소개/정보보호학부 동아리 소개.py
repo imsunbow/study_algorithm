73f3890289906ded452a2cc375b226f4398f831b
imsunbow <145563331+imsunbow@users.noreply.github.com>
2023-11-20 19:55:26 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,30 @@
+#백준 28691
+
+T = input()
+
+if T == 'M':
+    print("MatKor")
+elif T == 'W':
+    print("WiCys")
+elif T == 'C':
+    print("CyKor")
+elif T == 'A':
+    print("AlKor")
+elif T == '$':
+    print("$clear")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
