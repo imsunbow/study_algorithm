ea41c3805b283b371a978cd98e982c7a81893716
imsunbow <145563331+imsunbow@users.noreply.github.com>
2023-11-22 18:48:48 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,14 @@
+#백준 5086
+
+while True:
+    a, b = map(int, input().split())
+
+    if a == 0 and b == 0:
+        break
+
+    if a % b == 0:
+        print("multiple")
+    elif b % a == 0:
+        print("factor")
+    elif a % b != 0:
+        print("neither")
