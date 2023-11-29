5292f23609e9ac91ffbaedd65b01333fa0b7ec26
imsunbow <145563331+imsunbow@users.noreply.github.com>
2023-11-29 12:25:12 +0900

    [Silver III] Title: 소수 구하기, Time: 1040 ms, Memory: 111464 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+#백준 1929
+
+M, N = map(int,input().split())
+
+for num in range(M, N+1):
+    if num ==1:
+        continue
+    for i in range(2, int(num**0.5)+1):
+        if num % i ==0:
+            break
+    else:
+        print(num)
+
+
