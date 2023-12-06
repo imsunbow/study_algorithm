b4c2195fa669a789e603ddf3620d0de71c007e53
imsunbow <145563331+imsunbow@users.noreply.github.com>
2023-12-07 02:17:24 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+#백준 2445: 별 찍기 8
+
+n = int(input())
+
+for i in range(1,n+1):
+    print("*" * i + " " * 2 * (n - i) + "*" * i)
+
+for j in range(1, n+1):
+    print("*" * (n - j) + " " * 2 * j + "*" * (n - j))
