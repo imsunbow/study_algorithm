7a6ad1ff122ab26fcd40310095a990ec51fe78f1
imsunbow <145563331+imsunbow@users.noreply.github.com>
2023-12-30 09:43:12 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -0,0 +1,18 @@
+#백준 2755: 이번학기 평점은 몇점?
+
+sum = 0
+rate = 0
+
+rating = {"A+": 4.3, "A0": 4.0, "A-":3.7, "B+": 3.3, "B0": 3.0, "B-":2.7, "C+": 2.3, "C0": 2.0, "C-":1.7, "D+": 1.3, "D0": 1.0, "D-":0.7, "F": 0.0}
+
+N = int(input())
+for _ in range(N):
+    subject, score, grade = input().split()
+    if grade =='P':
+        continue
+    rate += float(score) * rating[grade]
+    sum += float(score)
+
+total_grade = rate/sum
+
+print("%.2f" % (round(total_grade + 10**-10, 2)))
