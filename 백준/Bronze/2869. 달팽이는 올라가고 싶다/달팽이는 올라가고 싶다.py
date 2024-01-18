25e625353a4bf7934187403eeda53a9838c96086
imsunbow <145563331+imsunbow@users.noreply.github.com>
2024-01-18 16:42:54 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,5 +1,10 @@
-import math
-A,B,V= map(int,input().split())
-    
-N= math.ceil((V-B)/(A-B))
-print(N)
+#백준 2869: 달팽이는 올라가고 싶다
+
+A,B,V = map(int,input().split())
+
+T = (V-B) // (A-B) #계산식
+
+if (V-B) % (A-B) == 0:
+    print(int(T))
+else:
+    print(int(T)+1)
