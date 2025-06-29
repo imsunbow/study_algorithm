a81d9be1e793baf6adb6f716d1f3c8c3281e27ac
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-06-29 17:57:24 +0900

    [Platinum II] Title: 마지막 팩토리얼 수2, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Platinum/2554. 마지막 팩토리얼 수2/마지막 팩토리얼 수2.py
+++ 백준/Platinum/2554. 마지막 팩토리얼 수2/마지막 팩토리얼 수2.py
@@ -1,28 +1,3 @@
-def power(a, b, mod):
-    result = 1
-    a = a % mod
-    while b > 0:
-        if b % 2 == 1:
-            result = (result * a) % mod
-        a = (a * a) % mod
-        b //= 2
-    return result
-
-def last_nonzero_digit(n):
-    if n < 5:
-        return [1, 1, 2, 6, 4][n]
-    
-    # mathmetical recursion
-    q = n // 5
-    r = n % 5
-    return (last_nonzero_digit(q) * power(2, q, 10) * [1, 1, 2, 6, 4][r]) % 10
-
-# input: up to 100 digits as a string
-import sys
-input = sys.stdin.readline
-
-n_str = input().strip()
-
-# Convert the string input to an integer
-n = int(n_str)
-print(last_nonzero_digit(n))
+n=int(input());s=1
+while n:s*=[1,1,2,6,4][n%5]*2**((n:=n//5)%4)
+print(s%10)
