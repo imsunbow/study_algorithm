efc0d709b3326d5e1c1db70363ecf4ce5c2e58db
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-06 14:41:20 +0900

    [Gold IV] Title: 나누기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/4379. 나누기/나누기.py
+++ 백준/Gold/4379. 나누기/나누기.py
@@ -1,26 +1,17 @@
-import sys
-import math
+import sys,math
 input = sys.stdin.readline
-
 while True:
-    line = input().split()
-    if not line:
-        break
-    a, b, c = map(int, line)
-    expr = f'({a}^{b}-1)/({a}^{c}-1)'
-    
-    # validation check
+    l = input().split()
+    if not l:break
+    a, b, c = map(int, l)
+    s = f'({a}^{b}-1)/({a}^{c}-1)'
     if a != 1 and b % c == 0:
-        # estimate the length of the input
         if round((b - c) * math.log10(a), 10) < 100:
             p = b // c
-            total = 0
-            # sum of by using for statement
+            t = 0
             for i in range(p):
-                total += a ** (c * i)
-            # check the length
-            if len(str(total)) < 100:
-                print(expr, total)
+                t += a ** (c * i)
+            if len(str(t)) < 100:
+                print(s, t)
                 continue
-
-    print(expr, 'is not an integer with less than 100 digits.')
+    print(s, 'is not an integer with less than 100 digits.')
