834d400ba7a92c710cf8080045c8732b371da5c4
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-06 09:46:53 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+school = input()
+
+if school[0] == 'N':
+    print("North London Collegiate School")
+elif school[0] == 'B':
+    print("Branksome Hall Asia")
+elif school[0] == 'K':
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
