3c72a91ee694bc807c3620b363bb31a581be4904
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-09 14:11:39 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
@@ -0,0 +1,11 @@
+# 백준 2864: 5와 6의 차이
+
+a,b = input().split()
+
+min_a = int(a.replace('6', '5'))
+min_b = int(b.replace('6', '5'))
+
+max_a = int(a.replace('5', '6'))
+max_b = int(b.replace('5', '6'))
+
+print(min_a + min_b, max_a + max_b)
