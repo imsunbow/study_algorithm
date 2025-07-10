ef78fa6d04295e2be1641a03bab329b7fbbe7c6b
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-10 13:20:15 +0900

    [Bronze IV] Title: Practice: Roll Call, Time: 152 ms, Memory: 110812 KB -BaekjoonHub

---
+++ 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.py
@@ -0,0 +1,6 @@
+# 백준 9306: practice : roll call
+
+for i in range(int(input())):
+    last_name = input().strip()
+    first_name = input().strip()
+    print(f"Case {i+1}: {first_name}, {last_name}")
