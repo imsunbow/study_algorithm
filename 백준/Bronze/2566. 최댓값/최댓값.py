1c373ead84272f2ea98607c7866a73e09127e009
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-10 16:44:53 +0900

    [Bronze III] Title: 최댓값, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,22 @@
+#백준 2566: 최댓값
+
+table = [list(map(int,input().split())) for _ in range(9)]
+
+#초기값 세팅
+max_num = 0
+max_row, max_col = 0,0
+
+#비교하여 최댓값 출력
+for row in range(9):
+    for col in range(9):
+        if max_num<= table[row][col]:
+            max_row = row + 1
+            max_col = col + 1
+            max_num = table[row][col]
+
+
+print(max_num)
+print(max_row,max_col)
+
+
+
