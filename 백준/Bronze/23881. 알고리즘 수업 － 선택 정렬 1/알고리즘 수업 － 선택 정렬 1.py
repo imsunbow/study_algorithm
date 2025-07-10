e0a1ddba865535d50bd795e654be9a8fc72e3f51
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-10 13:10:26 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 256 ms, Memory: 110924 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
@@ -0,0 +1,19 @@
+# 백준 23881 : 알고리즘 수업1
+
+n,k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+cnt = 0
+
+for i in range(n-1,0,-1):
+    index = i
+    for j in range(i-1, -1, -1):
+        if arr[j] > arr[index]:
+            index = j
+    if index != i:
+        arr[i], arr[index] = arr[index], arr[i] # swap
+        cnt += 1
+        if cnt == k:
+            print(arr[index],arr[i])
+            exit()
+print(-1)
