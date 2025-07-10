d169b2793d6dc14e69f8394d754c48afdd9b1ab3
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-11 00:38:11 +0900

    [Gold II] Title: 피보나치 수 3, Time: 192 ms, Memory: 208688 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,12 @@
+#백준 2749: 피보나치 수 3
+
+n = int(input())
+
+fibo = [0,1]
+p = (1000000//10)*15
+
+for i in range(2,p):
+    fibo.append(fibo[i-1]+fibo[i-2])
+    fibo[i] %= 1000000
+
+print(fibo[n%p])
