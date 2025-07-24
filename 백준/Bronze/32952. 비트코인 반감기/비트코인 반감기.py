4c6afc0b83c29e6647ed8d8837ee2ee1687f31ae
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-24 12:29:46 +0900

    [Bronze II] Title: 비트코인 반감기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32952. 비트코인 반감기/비트코인 반감기.py
+++ 백준/Bronze/32952. 비트코인 반감기/비트코인 반감기.py
@@ -1,11 +1,3 @@
-# 백준 32952 : 비트코인 반감기
-
-r, k, m = map(int,input().split())
-
-i = m // k
-
-while i > 0 and r > 0:
-    r >>= 1 # bit moving
-    i -= 1
-    
-print(r)
+r,k,m=map(int, input().split())
+halvings=min(m//k,r.bit_length()) 
+print(r>>halvings)
