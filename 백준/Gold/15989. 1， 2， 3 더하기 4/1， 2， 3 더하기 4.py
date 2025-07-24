09dcb73536a4949171ec83c92bd8b91f5c56d618
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-24 13:12:26 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,18 +1,10 @@
-# 백준 1 2 3 더하기 4 
-
 import sys
 input = sys.stdin.readline
-
-dp = [1] * 10001 # reset dp data
-
-for i in range(2,10001): # make 2
+dp = [1] * 10001  
+for i in range(2, 10001):  
     dp[i] += dp[i - 2]
-    
-for i in range(3,10001): # make 3 (* index should be from 3 )
+for i in range(3, 10001): 
     dp[i] += dp[i - 3]
-
-t = int(input())
-
-for _ in range(t):
+for _ in range(int(input())):
     n = int(input())
-    print(dp[n])        
+    print(dp[n])
