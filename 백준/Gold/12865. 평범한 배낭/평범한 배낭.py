ddd3b9cea1f4ded3bd7435a8a46dd5eed4221586
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-25 18:00:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 2680 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,12 @@
-#백준 12865: 평범한 배낭
+import sys
+input = sys.stdin.readline
 
-n,k = map(int,input().split())
-dp = [[0]*(k+1) for _ in range(n+1)]
+n, k = map(int, input().split())
+dp = [0] * (k + 1)
 
-
-for i in range(1,n+1):
-    w,v = map(int,input().split())
-    for j in range(1,k+1): #무게 1->k까지
-        if j < w:  #j가 w보다 작으면 
-            dp[i][j] = dp[i-1][j] #이전값을 그대로
-        else: #j가 w보다 크거나 같으면
-            dp[i][j] = max(dp[i-1][j],dp[i-1][j-w]+v) #이전값 or 이전값에서 w를 뺀값+현재값 중 큰값으로 업데이트
-            
-print(dp[n][k])
+for _ in range(n):
+    w, v = map(int,input().split())
+    for j in range(k, w-1, -1):
+        dp[j] = max(dp[j], dp[j - w] + v)
+        
+print(dp[k])
