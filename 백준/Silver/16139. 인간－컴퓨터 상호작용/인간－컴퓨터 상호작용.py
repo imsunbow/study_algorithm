b5883a30b5db86b5a3597bcc6198b64bd3203609
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-07-28 17:01:30 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 48 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,16 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-s = input()
+s = input().strip()
 q = int(input())
+
+count = [[0] * (len(s) + 1) for _ in range(26)] # make table in advance
+
+for i in range(1, len(s) + 1):
+    char_idx = ord(s[i - 1]) - ord('a')
+    for j in range(26):
+        count[j][i] = count[j][i - 1]
+    count[char_idx][i] += 1
 
 for _ in range(q):
     alpha, l, r = input().split()
     l = int(l)
     r = int(r)
-    
-    count = 0    
-    for i in range(l, r + 1):
-        if s[i] == alpha:
-            count += 1
-    print(count)
+    idx = ord(alpha) - ord('a')
+    print(count[idx][r + 1] - count[idx][l])
