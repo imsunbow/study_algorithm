9a7c47b250543c6d835ab33cf8c6810608798d26
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-08-02 12:59:36 +0900

    [Silver II] Title: 유기농 배추, Time: 116 ms, Memory: 239720 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,34 @@
-#백준 1012: 유기농 배추
-
 import sys
-sys.setrecursionlimit(10**4)
+sys.setrecursionlimit(10**5)
 
-def dfs(x,y):
-    dx = [0,0,-1,1]
-    dy = [1,-1,0,0]
+input = sys.stdin.readline
+
+def dfs(row, col):
+    dx = [-1, 1, 0, 0]  
+    dy = [0, 0, -1, 1]  
 
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx = row + dx[i]
+        ny = col + dy[i]
 
-        #배추이면 > -1로 표시
-        if (0 <= nx < m) and (0 <= ny < n) and box[ny][nx] == 1:
-            box[ny][nx] = -1
+        if 0 <= nx < n and 0 <= ny < m and field[nx][ny] == 1:
+            field[nx][ny] = -1
             dfs(nx, ny)
 
 t = int(input())
 
 for _ in range(t):
-    m, n, k = map(int, input().split())
-    box = [[0 for _ in range(m)] for _ in range(n)]
+    m, n, k = map(int, input().split()) 
+    field = [[0] * m for _ in range(n)]
 
     for _ in range(k):
-        x,y = map(int,input().split())
-        box[y][x] = 1
+        x, y = map(int, input().split())
+        field[y][x] = 1
 
     cnt = 0
-    for i in range(m):
-        for j in range(n):
-            if box[j][i] == 1:
-                dfs(i,j)
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] == 1:
+                dfs(i, j)
                 cnt += 1
-
     print(cnt)
-
-
-
-
-
