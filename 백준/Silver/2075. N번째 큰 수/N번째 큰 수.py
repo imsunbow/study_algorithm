febf29a30eb6f2eb03ffc5641f22afef6e8b67c4
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-08-10 13:47:35 +0900

    [Silver III] Title: N번째 큰 수, Time: 456 ms, Memory: 134056 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -4,15 +4,21 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
 heap = []
+for col in range(n):
+    heapq.heappush(heap, (-board[n-1][col], n-1, col))
+    
+visited = set()
 
 for _ in range(n):
-    nums = list(map(int, input().split()))
-    for num in nums:
-        if len(heap) < n:
-            heapq.heappush(heap, num)
-        else:
-            if num > heap[0]:
-                heapq.heappushpop(heap, num)
-
-print(heap[0])
+    value, row, col = heapq.heappop(heap)
+    result = -value
+    
+    # if not visited
+    if row > 0 and (row-1, col) not in visited:
+        heapq.heappush(heap, (-board[row-1][col], row-1, col)) # push minimum heap
+        visited.add((row-1, col))
+        
+print(result)
