142b4f55bd0d6d0b8cf8b6355b1f42f7bcba1b6a
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-08-24 20:15:18 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 724 ms, Memory: 110952 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    cnt = 0
+    
+    # sol : brute force 
+    for x in range(1, a+1):
+        for y in range(1, b+1):
+            for z in range(1, c+1):
+                if (x % y) == (y % z) == (z % x):
+                    cnt += 1
+                    
+    print(cnt)
