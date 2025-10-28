9388f6585f986d8407ef0b275afb11b753f028f0
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-10-28 09:59:48 +0900

    [Silver V] Title: 숫자 게임, Time: 112 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/2303. 숫자 게임/숫자 게임.py
+++ 백준/Silver/2303. 숫자 게임/숫자 게임.py
@@ -1,22 +1,24 @@
+import sys
 from itertools import combinations as comb
 
-N = int(input())
-max_num = []
+input = sys.stdin.readline
 
-for _ in range(N):
-    check = []
-    card = list(map(int,input().split()))
-    numbers = list(comb(card, 3))
-    for num in numbers :
-        c = sum(num) % 10 # calculate mod 10
-        check.append(c)
-    max_num.append(max(check))
+personal_max_num = []
 
+n = int(input())
 
-check_max = []
+for _ in range(n):
+    personal_card = []
+    card_lst= list(map(int, input().split()))
 
-for i in range(len(max_num)):
-    if max_num[i] == max(max_num) :
-        check_max.append(i + 1) # calculate player number
+    card_comb = list(comb(card_lst, 3))
+    personal_card.append(max(sum(comb) % 10 for comb in card_comb))
+    personal_max_num.append(personal_card[0])
 
-print(max(check_max))
+highest_score_players = []
+
+for i in range(len(personal_max_num)):
+    if personal_max_num[i] == max(personal_max_num):
+        highest_score_players.append(i + 1) # save index
+
+print(max(highest_score_players))
