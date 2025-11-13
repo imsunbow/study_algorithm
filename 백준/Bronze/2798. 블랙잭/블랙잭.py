55d02501f2179aa5901aab9eaf6704900a3df8a7
imsunbow <145563331+imsunbow@users.noreply.github.com>
2025-11-13 17:25:59 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 121428 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+res = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            card_sum = lst[i] + lst[j] + lst[k]
+            
+            if card_sum <= m:
+                lst.append(card_sum)
+                
+print(max(lst[n:]))
