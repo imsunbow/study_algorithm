f6855c1727f720eec7478c140039a5f52d548042
imsunbow <145563331+imsunbow@users.noreply.github.com>
2026-02-16 01:39:18 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 136 ms, Memory: 110712 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,13 @@
+while True:
+    w = input()
+    
+    if w == 'end':
+        break
+    
+    if w == 'animal':
+        print('Panthera tigris')
+    elif w == 'flower':
+        print('Forsythia koreana')
+    elif w == 'tree':
+        print('Pinus densiflora')
+        
