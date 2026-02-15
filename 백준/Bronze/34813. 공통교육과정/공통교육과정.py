77ee0864ee6be6b2ddc51bff25f7708b2e247bb4
imsunbow <145563331+imsunbow@users.noreply.github.com>
2026-02-16 01:25:39 +0900

    [Bronze V] Title: 공통교육과정, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,10 @@
+w = input()
+
+if w[0] == 'F':
+    print('Foundation')
+elif w[0] == 'C':
+    print('Claves')
+elif w[0] == 'V':
+    print('Veritas')
+elif w[0] == 'E':
+    print('Exploration')
